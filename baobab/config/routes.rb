9cc9a5ae04db881dc2a9c456ba6d5598ca690c29
febero <felix@rouillard.info>
2014-02-26 11:23:40 +0100

    Add User model with Devise

--- baobab/config/routes.rb
+++ baobab/config/routes.rb
@@ -1,5 +1,10 @@
 Baobab::Application.routes.draw do
+
+  devise_for :users
+  root :to => "organizations#index"
+
   resources :organizations
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
