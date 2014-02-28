2b5bba4de31213a94ee341396d27402b444c9821
Beub <benoitchibrac@gmail.com>
2014-02-28 11:21:02 +0100

    develop commit

--- baobab/db/schema.rb
+++ baobab/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140227100449) do
+ActiveRecord::Schema.define(version: 20140227143819) do
 
   create_table "artists", force: true do |t|
     t.string   "name"
@@ -38,6 +38,13 @@ ActiveRecord::Schema.define(version: 20140227100449) do
     t.integer  "depth"
     t.integer  "estimation"
     t.integer  "artist_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "customers", force: true do |t|
+    t.string   "name"
+    t.string   "id_card_number"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
