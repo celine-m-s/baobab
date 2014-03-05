0fc9a4c58ae69eaebff286545a1d1a040bb0f1be
Céline Martinet Sanchez <celine.martinet.sanchez@gmail.com>
2014-03-06 00:18:06 +0100

    avec la gem simple_form

--- baobab/app/controllers/application_controller.rb
+++ baobab/app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
 
   # Includes Authorization mechanism
   include Pundit
+  layout "application"
 
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
--- baobab/app/controllers/pages_controller.rb
+++ baobab/app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ActionController::Base
 	protect_from_forgery with: :exception
+  layout "pages"
 
   def index
 
